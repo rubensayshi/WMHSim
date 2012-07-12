7bc1bcabeb9dc702279ed82ff2c553d265e21d88
Ruben de Vries <ruben@rubensayshi.com>
2012-07-12 17:42:09 +0200

    seperating the fixed cases, to make way to an interface

---
+++ v0.4/tests/carni_vs_feral.php
@@ -0,0 +1,32 @@
+<?php
+
+use WMHSim\Factions\Legion\eThagrosh;
+
+ini_set('display_errors', 'On');
+define('IS_CLI', php_sapi_name() == 'cli');
+define('DEBUG',  false);
+
+require __DIR__.'/../autoload.php';
+
+use WMHSim\SimScenario;
+
+$laps    = 1000;
+$laps    = DEBUG ? 1 : $laps;
+
+ob_start();
+
+$scenario = new SimScenario(new \WMHSim\Factions\Legion\Carnivean(), new \WMHSim\Factions\Circle\FeralWarpwolf());
+
+$scenario->setBoostAttack(false);
+$scenario->setBoostDamage(false);
+$scenario->setChargeAttack(false);
+$scenario->setLaps($laps);
+$scenario->setDebug(DEBUG);
+$scenario->getAttacker()->addBuff('incite');
+$scenario->getAttacker()->addBuff('chiller');
+$scenario->getDefender()->addBuff('warp-str');
+
+$scenario->run();
+
+$output = ob_get_clean();
+echo IS_CLI ? $output : str_replace("\n", "<br />", $output);
---
+++ v0.4/tests/rhyas_vs_feral.php
@@ -0,0 +1,30 @@
+<?php
+
+use WMHSim\Factions\Legion\eThagrosh;
+
+ini_set('display_errors', 'On');
+define('IS_CLI', php_sapi_name() == 'cli');
+define('DEBUG',  false);
+
+require __DIR__.'/../autoload.php';
+
+use WMHSim\SimScenario;
+
+$laps    = 1000;
+$laps    = DEBUG ? 1 : $laps;
+
+ob_start();
+
+$scenario = new SimScenario(new \WMHSim\Factions\Legion\Rhyas(), new \WMHSim\Factions\Circle\FeralWarpwolf());
+
+$scenario->setBoostAttack(true);
+$scenario->setBoostDamage(true);
+$scenario->setChargeAttack(false);
+$scenario->setLaps($laps);
+$scenario->setDebug(DEBUG);
+$scenario->getDefender()->addBuff('warp-arm');
+
+$scenario->run();
+
+$output = ob_get_clean();
+echo IS_CLI ? $output : str_replace("\n", "<br />", $output);
