b66ec7dd3bd72002970d6244353b3807478a2fde
Ruben de Vries <ruben@rubensayshi.com>
2012-07-16 11:28:54 +0200

    removed old tryout versions, moved v0.4 into main dir to work with

--- v0.3/sim.js
+++
@@ -1,7 +0,0 @@
-var WMHSim			= require('./js/WMHSim');
-var WMHSimLegion	= require('./js/WMHSimLegion');
-
-var sim = new WMHSim();
-sim.simulate(WMHSimLegion.carnivean, WMHSimLegion.shredder);
-
-console.log('-----');
